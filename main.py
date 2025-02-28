75987103ac9913e5bc7264e9021ac0b7c41938b1
Maxim-Borisov7 <maxvadbor@mail.ru>
2025-03-01 00:07:19 +0500

    Внес правки в main

--- main.py
+++ main.py
@@ -8,16 +8,20 @@ def main():
     while True:
         updates_message = get_updates(offset)
         if "result" not in updates_message:
-            return
+            continue
+
         for message in updates_message["result"]:
             offset = message["update_id"] + 1
-            if "message" in message and "text" in message["message"]:
-                chat_id = message["message"]["chat"]["id"]
-                text = message["message"]["text"]
-                if text == "/start":
-                    get_start(chat_id)
-                    continue
-                respond(chat_id, text)
+
+            if "message" not in message and "text" not in message["message"]:
+                continue
+            chat_id = message["message"]["chat"]["id"]
+            text = message["message"]["text"]
+
+            if message["message"]["text"] == "/start":
+                get_start(chat_id)
+                continue
+            respond(chat_id, text)
 
 
 if __name__ == "__main__":
